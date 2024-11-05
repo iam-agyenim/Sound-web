2de003b7cb01d52d89e154d73d376b9a18ca1221
Richmond Agyenim-Boateng <165687064+iam-agyenim@users.noreply.github.com>
2024-11-05 14:09:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -13,15 +13,3 @@ audioBtn.addEventListener('click', () => {
     }
 });
 
-const carousel = document.querySelector('.carousel');
-const images = document.querySelectorAll('.carousel-image');
-let index = 0;
-
-// Function to show the next image
-function showNextImage() {
-    index = (index + 1) % images.length;  // Cycle back to the start
-    carousel.style.transform = `translateX(-${index * 100}%)`;
-}
-
-// Auto-slide every 5 seconds
-setInterval(showNextImage, 5000);
